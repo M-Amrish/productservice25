71390e9fbd3ec06fd412fa78c3e813e4c6f66985
Amrish <amrish.m004@gmail.com>
2025-02-22 23:08:13 +0530

    ProductService

---
+++ src/main/java/dev/amrish/productservice/Productservice25Application.java
@@ -0,0 +1,13 @@
+package dev.amrish.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservice25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productservice25Application.class, args);
+	}
+
+}
